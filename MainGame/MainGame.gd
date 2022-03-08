f0119a1d471e2ab089a135301efe753167ced8e1
0384359 <0384359@schoolsnet.act.edu.au>
2022-03-08 15:00:36 +1100

    dog now barks

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,12 +1,22 @@
 extends Control
 
+export(int) var countdownMax
+var currentTimer
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	set_process(true)
+	currentTimer = countdownMax
+	$HUD/Countdown.text = str(currentTimer)
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("GAME OVER")
 
 func _process(delta):
-	pass
+	$HUD/CurrentScore.text = str (GlobalVariables.scoringInformation["currentScore"])
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Player/Player.gd
+++ Player/Player.gd
@@ -18,9 +18,17 @@ func _physics_process(delta):
 		move_and_collide(Vector2(0, -movement_speed * delta))
 	if Input.is_action_pressed("ui_down"):
 		move_and_collide(Vector2(0, movement_speed * delta))
-			
+	
+	if Input.is_action_pressed("ui_left"):
+		move_and_collide(Vector2(-movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_right"):
+		move_and_collide(Vector2(movement_speed * delta, 0))
+
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
